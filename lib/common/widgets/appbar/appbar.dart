40273338394a5552e97f0811c7ec9da3d80b1ba9
Kartikey Raghav <kartikeyraghav2006@gmail.com>
2025-02-15 23:34:51 +0530

    Made changes in appbar icon

--- lib/common/widgets/appbar/appbar.dart
+++ lib/common/widgets/appbar/appbar.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:opulent/utils/constants/colors.dart';
 import 'package:opulent/utils/constants/sizes.dart';
 import 'package:opulent/utils/device/device_utility.dart';
+import 'package:opulent/utils/helpers/helper_functions.dart';
 
 class OpulentAppbar extends StatelessWidget implements PreferredSizeWidget {
-  const OpulentAppbar(
-      {super.key,
-      this.title,
-      this.showBackArrow = false,
-      this.leadingIcon,
-      this.actions,
-      this.leadingOnPressed});
+  const OpulentAppbar({
+    super.key,
+    this.title,
+    this.showBackArrow = false,
+    this.leadingIcon,
+    this.actions,
+    this.leadingOnPressed,
+  });
 
   final Widget? title;
   final bool showBackArrow;
@@ -21,6 +24,7 @@ class OpulentAppbar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dark = OpulentHelperFunctions.isDarkMode(context);
     return Padding(
       padding: EdgeInsets.symmetric(
         horizontal: OpulentSizes.md,
@@ -29,10 +33,20 @@ class OpulentAppbar extends StatelessWidget implements PreferredSizeWidget {
         automaticallyImplyLeading: false,
         leading: showBackArrow
             ? IconButton(
-                onPressed: () => Get.back(), icon: Icon(Iconsax.arrow_left))
+                onPressed: () => Get.back(),
+                icon: Icon(
+                  Iconsax.arrow_left,
+                  color: dark ? OpulentColors.white : OpulentColors.black,
+                ),
+              )
             : leadingIcon != null
                 ? IconButton(
-                    onPressed: leadingOnPressed, icon: Icon(leadingIcon))
+                    onPressed: leadingOnPressed,
+                    icon: Icon(
+                      leadingIcon,
+                      color: dark ? OpulentColors.white : OpulentColors.black,
+                    ),
+                  )
                 : null,
         title: title,
         actions: actions,
